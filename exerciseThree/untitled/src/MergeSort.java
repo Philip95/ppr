fe81a487f2860eab224aec3dfaa7f319157baef3
Philip95 <philip.strobach@gmail.com>
2023-03-21 14:11:53 +0100

    Class Structure

---
+++ exerciseThree/untitled/src/MergeSort.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[] numbers = {11, 123, 5, 34, 90, 22, 1, 70};
+        sequentialMergeSort(numbers);
+    }
+
+    private static void sequentialMergeSort(int [] numbers) {
+
+    }
+}
---
+++ exerciseThree/untitled/src/Quicksort.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Quicksort {
+    public static void main(String[] args) {
+        int[] numbers = {11, 123, 5, 34, 90, 22, 1, 70};
+
+        sequentialQuickSort(numbers);
+
+    }
+
+
+
+    private static void sequentialQuickSort(int [] numbers) {
+
+    }
+
+
+}
