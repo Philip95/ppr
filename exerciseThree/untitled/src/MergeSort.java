272a6111c2713ab046b605cb688b5c2d44362f30
Philip95 <philip.strobach@gmail.com>
2023-03-21 16:09:10 +0100

    Add some method stuff

--- exerciseThree/untitled/src/MergeSort.java
+++ exerciseThree/untitled/src/MergeSort.java
@@ -1,38 +1,25 @@
 public class MergeSort {
     public static void main(String[] args) {
-        //System.out.println("Given Array");
-        //printArray(MergeSortParallel.numbers());
-
         MergeSort ob = new MergeSort();
         long start = System.nanoTime();
         ob.sort(MergeSortParallel.numbers(), 0, MergeSortParallel.numbers().length - 1);
-        System.out.println("\n "+ (System.nanoTime() - start) / 1000000.0 + "[msec]");
-
-        //System.out.println("\nSorted array");
-        //printArray(numbers);
+        System.out.println("\n "+ (System.nanoTime() - start) / 1000000.0 + " ms");
     }
 
-    void merge(int numbers[], int l, int m, int r) {
-        // Find sizes of two subarrays to be merged
+    private void merge(int[] numbers, int l, int m, int r) {
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* Create temp arrays */
         int[] left = new int[n1];
         int[] right = new int[n2];
 
-        /*Copy data to temp arrays*/
         System.arraycopy(numbers, l, left, 0, n1);
 
         for (int j = 0; j < n2; ++j)
             right[j] = numbers[m + 1 + j];
 
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarray array
         int k = l;
         while (i < n1 && j < n2) {
             if (left[i] <= right[j]) {
@@ -46,14 +33,12 @@ public class MergeSort {
             k++;
         }
 
-        /* Copy remaining elements of Left[] if any */
         while (i < n1) {
             numbers[k] = left[i];
             i++;
             k++;
         }
 
-        /* Copy remaining elements of Right[] if any */
         while (j < n2) {
             numbers[k] = right[j];
             j++;
@@ -61,21 +46,14 @@ public class MergeSort {
         }
     }
 
-    void sort(int arr[], int l, int r) {
+    private void sort(int[] arr, int l, int r) {
         if (l < r) {
-            // Find the middle point
             int m = l + (r - l) / 2;
 
-            // Sort first and second halves
             sort(arr, l, m);
             sort(arr, m + 1, r);
 
-            // Merge the sorted halves
             merge(arr, l, m, r);
         }
-    }
-
-    static void printArray(int arr[]) {
-        for (int j : arr) System.out.print(j + " ");
     }
 }
--- exerciseThree/untitled/src/MergeSortParallel.java
+++ exerciseThree/untitled/src/MergeSortParallel.java
@@ -13,11 +13,11 @@ public class MergeSortParallel extends RecursiveAction {
 
         ForkJoinPool pool = new ForkJoinPool();
         pool.submit(new MergeSortParallel(array, 0, array.length)).join();
-        System.out.println("\n " + (System.nanoTime() - start) / 1000000.0 + "[msec]");
+        System.out.println("\n " + (System.nanoTime() - start) / 1000000.0 + " ms");
 
     }
 
-    static int[] numbers() {
+    public static int[] numbers() {
         int[] numbers = new int[10000000];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int) (Math.random() * 10000000);
@@ -25,7 +25,7 @@ public class MergeSortParallel extends RecursiveAction {
         return numbers;
     }
 
-    MergeSortParallel(int[] array, int low, int high) {
+    public MergeSortParallel(int[] array, int low, int high) {
         this.array = array;
         this.low = low;
         this.high = high;
@@ -33,6 +33,7 @@ public class MergeSortParallel extends RecursiveAction {
 
     public void compute() {
         int size = high - low;
+
         if (size <= 8) {
             Arrays.sort(array, low, high);
         } else {
@@ -46,12 +47,16 @@ public class MergeSortParallel extends RecursiveAction {
         if (array[middle - 1] < array[middle]) {
             return;
         }
+
         int copySize = high - low;
         int copyMiddle = middle - low;
         int[] copy = new int[copySize];
+
         System.arraycopy(array, low, copy, 0, copy.length);
+
         int p = 0;
         int q = copyMiddle;
+
         for (int i = low; i < high; ++i) {
             if (q >= copySize || (p < copyMiddle && copy[p] < copy[q])) {
                 array[i] = copy[p++];
