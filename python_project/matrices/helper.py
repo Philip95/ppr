d25f5ab39e6ce0d6de330f10742a74f177416542
Philip95 <philip.strobach@gmail.com>
2023-04-12 15:17:15 +0200

    .

--- python_project/matrices/helper.py
+++ python_project/matrices/helper.py
@@ -1,6 +1,10 @@
 import random
+
+
 def generate_matrices(rows, cols, min_value, max_value):
     A = [[random.randint(min_value, max_value) for _ in range(cols)] for _ in range(rows)]
     B = [[random.randint(min_value, max_value) for _ in range(cols)] for _ in range(rows)]
     return A, B
+
+
 matrix_A, matrix_B = generate_matrices(8, 8, 0, 10)
